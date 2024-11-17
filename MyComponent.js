7c48d677b1b7507b11e491e0ceda89de2fdf2f0d
Richjerk <puseletso55@gmail.com>
2024-11-17 19:37:58 +0200

    Add new files and update configuration for deployment

--- MyComponent.js
+++ MyComponent.js
@@ -1,3 +1,4 @@
+HEAD
 const MyComponent = React.lazy(() => import('./MyComponent'));
 
 function App() {
@@ -7,3 +8,14 @@ function App() {
         </Suspense>
     );
 }
+
+const MyComponent = React.lazy(() => import('./MyComponent'));
+
+function App() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <MyComponent />
+        </Suspense>
+    );
+}
+(Add new files and update configuration for deployment)
